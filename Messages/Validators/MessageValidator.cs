1fa7710f4ede7dab81b287ed27aa63a91b20a167
Mohamed Al Saeed <mohamedalsaied365@gmail.com>
2023-05-10 16:30:29 +0300

    EmailSender

--- Messages/Validators/MessageValidator.cs
+++ Messages/Validators/MessageValidator.cs
@@ -1,5 +1,9 @@
 using Common.Validators;
 public class MessageValidator : BaseViewModelValidator<MessageViewModel>
 {
-
+public MessageValidator()
+    {
+        RuleFor(m => m.MessageText).NotEmpty().NotNull().WithMessage("The field is Required");
+        RuleFor(m => m.Subject).NotEmpty().NotNull().WithMessage("The field is Required");
+    }
 }
